81c20dcbd3b40db4b7bc6889f2ee34cdbcaa5a4a
vitaliy.frolov <leventel8@yandex.ru>
2018-12-24 15:17:11 +0300

    csv

--- src/Collections.java
+++ src/Collections.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Collections {
 
@@ -12,8 +13,24 @@ public class Collections {
 
         String text = new String(data);
 
-        System.out.println(text);
+        //System.out.println(text);
+        ArrayList<String []> lineWords = new ArrayList<>();
 
+        String [] lines = text.split("\n");
+        for(String line : lines){
+            String [] words = line.split(",");
+            lineWords.add(words);
+        }
+
+        for(String [] words : lineWords) {
+            if(words[9].equals("1994")) {
+                for (String word : words) {
+                    System.out.print(word);
+                    System.out.print("!");
+                }
+                System.out.println("__");
+            }
+        }
 
     }
 }
